7c154df7643d51be71ec1654384ea61c98a641be
owenHochwald <owenhochwald@gmail.com>
2025-04-21 10:11:34 -0700

    update config.py

--- backend/app/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseSettings
-
-class Settings(BaseSettings) :
-    FRONTEND_URL: str = "*"  # Replace with frontend URL later
-    DATA_PATH: str = "app/data/houses.csv"
-    
-settings = Settings()
